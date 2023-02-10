881e246c6a0c6b7275a2ab9f113fd12e52f4ca9e
CleAyz <clementayz@gmail.com>
2023-02-11 04:04:39 +0800

    update styling

--- src/components/character/Character.tsx
+++ src/components/character/Character.tsx
@@ -43,7 +43,7 @@ const Character = () => {
   ];
   return (
     <div className="character-container">
-      <div className="content">Header - Skip QQ's Character</div>
+      <div className="content">Character</div>
       <div className="description">Description</div>
       <div className="card-container">
         {items.map((item, index) => (
